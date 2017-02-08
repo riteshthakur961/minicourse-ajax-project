d2423b6636ad661925bd688610a78f5b6a0f0bf1
riteshthakur961 <riteshthakur961@outlook.com>
2017-02-08 20:48:46 +0530

    Modified js/script.js

--- js/script.js
+++ js/script.js
@@ -20,8 +20,25 @@ function loadData() {
     $body.append('<img class="bgimg" src="'+streetviewUrl+'">');
 
     // YOUR CODE GOES HERE!
+    // load nytimes
+    // obviously, replace all the "X"s with your own API key
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=0d74b498b34b48e78cf8d0776de8ffb7';
+    $.getJSON(nytimesUrl, function(data){
+        //console.log(data);
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
-    //return false;
-};
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        }
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+    return false;
+}
 
 $('#form-container').submit(loadData);
